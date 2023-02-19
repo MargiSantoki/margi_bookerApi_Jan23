fd784be9c56bae2a856914c9b029155a04a5c68a
Margi Santoki <margisantoki@gmail.com>
2023-02-19 12:13:38 +0530

    Commit

--- src/test/java/testscripts/CreateBookingTest.java
+++ src/test/java/testscripts/CreateBookingTest.java
@@ -23,10 +23,7 @@ public class CreateBookingTest {
 	public void generateToken() {
 		RestAssured.baseURI = "https://restful-booker.herokuapp.com";
 
-		String payload = "{\r\n" 
-		+ "    \"username\" : \"admin\",\r\n" 
-				+ "    \"password\" : \"password123\"\r\n" 
-		+ "}";
+		String payload = "{\r\n" + "    \"username\" : \"admin\",\r\n" + "    \"password\" : \"password123\"\r\n" + "}";
 
 		RequestSpecification reqSpec = RestAssured.given();
 		reqSpec.baseUri("https://restful-booker.herokuapp.com");
@@ -83,13 +80,13 @@ public class CreateBookingTest {
 //		Assert.assertEquals(res.getStatusCode(), StatusCode.OK);
 //		System.out.println(res.getStatusLine());
 //	}
-	
+
 	@Test
 	public void createBookingTestWithPojo() {
 		BookingDates bookingDates = new BookingDates();
 		bookingDates.checkin = "2022-01-01";
 		bookingDates.checkout = "2022-01-05";
-		
+
 		payload = new CreateBookingRequest();
 		payload.firstname = "Margi";
 		payload.lastname = "Vegada";
@@ -97,14 +94,9 @@ public class CreateBookingTest {
 		payload.depositpaid = true;
 		payload.additionalneeds = "Breakfast";
 		payload.bookingdates = bookingDates;
-		
-		
-		Response res = RestAssured.given()
-		.headers("Content-Type", "application/json")
-		.headers("Accept","application/json")
-		.body(payload)
-		.when()
-		.post("/booking");
+
+		Response res = RestAssured.given().headers("Content-Type", "application/json")
+				.headers("Accept", "application/json").body(payload).when().post("/booking");
 		Assert.assertEquals(res.getStatusCode(), StatusCode.OK);
 //		Assert.assertTrue(Integer.valueOf(res.jsonPath().getInt("bookingid")) instanceof Integer);
 		bookingId = res.jsonPath().getInt("bookingid");
@@ -112,70 +104,59 @@ public class CreateBookingTest {
 		Assert.assertTrue(bookingId > 0);
 		validateResponse(res, payload, "booking.");
 	}
-	
-	@Test (priority = 1, enabled = false)
+
+	@Test(priority = 1, enabled = false)
 	public void getAllBookingTest() {
 //		int bookingId = 2522;
-		Response res = RestAssured.given()
-						.headers("Accept","application/json")
-						.when()
-						.get("/booking");
-		Assert.assertEquals(res.getStatusCode(),StatusCode.OK);
+		Response res = RestAssured.given().headers("Accept", "application/json").when().get("/booking");
+		Assert.assertEquals(res.getStatusCode(), StatusCode.OK);
 //		System.out.println(res.jsonPath());
 		List<Integer> listOfBookingId = res.jsonPath().getList("bookingid");
 //		System.out.println(listOfBookingId.size());
 		Assert.assertTrue(listOfBookingId.contains(bookingId));
 	}
-	
-	@Test (priority = 2, enabled = false)
+
+	@Test(priority = 2, enabled = false)
 	public void getBookingIdTest() {
-		Response res = RestAssured.given()
-						.headers("Accept","application/json")
-						.when()
-						.get("/booking/" + bookingId);
-		Assert.assertEquals(res.getStatusCode(),StatusCode.OK);
+		Response res = RestAssured.given().headers("Accept", "application/json").when().get("/booking/" + bookingId);
+		Assert.assertEquals(res.getStatusCode(), StatusCode.OK);
 		System.out.println(res.asPrettyString());
 		validateResponse(res, payload, "");
 	}
-	
-	@Test (priority = 2)
+
+	@Test(priority = 2)
 	public void getBookingIdPojoDeserializedTest() {
-		Response res = RestAssured.given()
-						.headers("Accept","application/json")
-						.when()
-						.get("/booking/" + bookingId);
-		Assert.assertEquals(res.getStatusCode(),StatusCode.OK);
+		Response res = RestAssured.given().headers("Accept", "application/json").when().get("/booking/" + bookingId);
+		Assert.assertEquals(res.getStatusCode(), StatusCode.OK);
 		System.out.println(res.asPrettyString());
 		CreateBookingRequest responseBody = res.as(CreateBookingRequest.class);
-		//payload : all details of request
-		//responseBody : all details of getBooking
+		// payload : all details of request
+		// responseBody : all details of getBooking
 		System.out.println(responseBody);
 		Assert.assertTrue(responseBody.equals(payload));
 	}
-	
-	@Test (priority = 3)
+
+	@Test(priority = 3)
 	public void updateBookingIdTest() {
 		payload.setFirstname("Harsh");
-		Response res = RestAssured.given()
-				.headers("Content-Type","application/json")
-				.headers("Accept","application/json")
-				.headers("Cookie","token=" + token)
-				.body(payload)
-				.when()
+		Response res = RestAssured.given().headers("Content-Type", "application/json")
+				.headers("Accept", "application/json").headers("Cookie", "token=" + token).body(payload).when()
 				.put("/booking/" + bookingId);
-		Assert.assertEquals(res.getStatusCode(),StatusCode.OK);
+		Assert.assertEquals(res.getStatusCode(), StatusCode.OK);
 		System.out.println(res.asPrettyString());
 		CreateBookingRequest responseBody = res.as(CreateBookingRequest.class);
 		Assert.assertTrue(responseBody.equals(payload));
 	}
-	
+
 	private void validateResponse(Response res, CreateBookingRequest payload, String object) {
 		Assert.assertEquals(res.jsonPath().getString(object + "firstname"), payload.getFirstname());
 		Assert.assertEquals(res.jsonPath().getString(object + "lastname"), payload.getLastname());
 		Assert.assertEquals(res.jsonPath().getInt(object + "totalprice"), payload.getTotalprice());
 		Assert.assertEquals(res.jsonPath().getBoolean(object + "depositpaid"), payload.isDepositpaid());
-		Assert.assertEquals(res.jsonPath().getString(object + "bookingdates.checkin"), payload.getBookingDates().getCheckin());
-		Assert.assertEquals(res.jsonPath().getString(object + "bookingdates.checkout"), payload.getBookingDates().getCheckout());
+		Assert.assertEquals(res.jsonPath().getString(object + "bookingdates.checkin"),
+				payload.getBookingDates().getCheckin());
+		Assert.assertEquals(res.jsonPath().getString(object + "bookingdates.checkout"),
+				payload.getBookingDates().getCheckout());
 		Assert.assertEquals(res.jsonPath().getString(object + "additionalneeds"), payload.getAdditionalneeds());
 	}
 }
